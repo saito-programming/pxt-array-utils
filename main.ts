a195652b6f88f2d0d2d5f8cc546ac68363557539
saito-programming <saito.programming.class@gmail.com>
2026-05-28 16:28:25 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 //% color="#D84315" icon="\uf0cb" block="配列ユーティリティ"
 namespace pxtArrayUtils {
 
-    //% block="%array=lists_create_empty の中からいちばん大きい数"
+    //% block="%array の中からいちばん大きい数"
     export function max(array: number[]): number {
         if (array.length == 0) {
             return 0
@@ -15,7 +15,7 @@ namespace pxtArrayUtils {
         return tmp
     }
 
-    //% block="%array=lists_create_empty の中でいちばん大きい数がある位置"
+    //% block="%array の中でいちばん大きい数がある位置"
     export function maxIndex(array: number[]): number {
         if (array.length == 0) {
             return -1 // 配列が空っぽのときは「なし（-1）」を返す
@@ -31,7 +31,7 @@ namespace pxtArrayUtils {
         return -1
     }
 
-    //% block="%array=lists_create_empty の中からいちばん小さい数"
+    //% block="%array の中からいちばん小さい数"
     export function min(array: number[]): number {
         if (array.length == 0) {
             return 0
@@ -45,7 +45,7 @@ namespace pxtArrayUtils {
         return tmp
     }
 
-    //% block="%array=lists_create_empty の中でいちばん小さい数がある位置"
+    //% block="%array の中でいちばん小さい数がある位置"
     export function minIndex(array: number[]): number {
         if (array.length == 0) {
             return -1 // 配列が空っぽのときは「なし（-1）」を返す
